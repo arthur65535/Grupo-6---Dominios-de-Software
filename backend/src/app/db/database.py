39766016dd4123ef4ac38305a231d31ca08fe37b
Karlos Daniel Pires da Silva <karlosdanielsilva7654@gmail.com>
2023-02-22 17:55:56 -0300

    feat: Containeriza o backend
    - Containeriza o backend com Docker e Docker Compose.
    
    - Altera os arquivos as configuração de conexão com o banco de dados
      para se adequar o banco de dados containerizado.

--- backend/src/app/db/database.py
+++ backend/src/app/db/database.py
@@ -10,8 +10,8 @@ from app.models.patient import *
 from app.models.transference_request import *
 
 USER = "postgres"
-PASSWORD = 123456789
-HOST = "localhost"
+PASSWORD = "development"
+HOST = "db"
 PORT = 5432
 DATABASE = "stp"
 
